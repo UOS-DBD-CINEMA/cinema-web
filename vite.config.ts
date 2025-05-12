07e3756a8644c905ba0aff0170c98b04a4bb2974
yschoi <yschoi9733@gmail.com>
2025-05-13 00:53:40 +0900

    chore: absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
   server: {
     proxy: {
       '^/api': {
