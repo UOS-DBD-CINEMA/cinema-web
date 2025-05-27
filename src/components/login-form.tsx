1566125059c0e2a9e4baf4955e5eb8413cedb94a
yschoi <yschoi9733@gmail.com>
2025-05-28 00:56:32 +0900

    feat: modify login form

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -34,7 +34,7 @@ export function LoginForm({
     [memberLogin, idRef, passwordRef],
   );
   return (
-    <div className={cn('flex flex-col gap-6', className)} {...props}>
+    <div className={cn('flex flex-col', className)} {...props}>
       <Card>
         <CardHeader>
           <CardTitle>Login</CardTitle>
