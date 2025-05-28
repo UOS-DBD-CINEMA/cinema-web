a0a39d845973a73cc6903f75ed2c13daf73dcdc6
yschoi <yschoi9733@gmail.com>
2025-05-28 18:02:15 +0900

    chore: modify Login page height overflow

--- src/routes/pages/Login.tsx
+++ src/routes/pages/Login.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/components/login-form';
 
 export function Login() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-[calc(100svh-(--spacing(14)))] w-full items-center justify-center p-6 md:p-10">
       <div className="w-sm">
         <LoginForm />
       </div>
