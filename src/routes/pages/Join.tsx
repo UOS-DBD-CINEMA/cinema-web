d285c2d68a2c270aa440faa6c632226c9bcd297c
yschoi <yschoi9733@gmail.com>
2025-06-12 21:04:51 +0900

    feat: add Join

--- src/routes/pages/Join.tsx
+++ src/routes/pages/Join.tsx
@@ -1,7 +1,16 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
 export function Join() {
   return (
-    <>
-      <h1>Join</h1>
-    </>
+    <div className="flex min-h-[calc(100svh-(--spacing(14)))] w-full items-center justify-center p-4 sm:py-8">
+      <div className="w-sm">
+        <Card>
+          <CardHeader>
+            <CardTitle>회원가입</CardTitle>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+      </div>
+    </div>
   );
 }
