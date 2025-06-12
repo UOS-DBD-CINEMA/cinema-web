41e6297b787c06c7757b06f24aa832c9e358d400
yschoi <yschoi9733@gmail.com>
2025-06-12 21:00:54 +0900

    chore: add getMovieDetailAPI

--- src/api/movie.api.ts
+++ src/api/movie.api.ts
@@ -4,6 +4,10 @@ export const getMoviesAPI = async () => {
   return await httpClient.get('/api/movies');
 };
 
+export const getMovieDetailAPI = async (movieId: number) => {
+  return await httpClient.get(`/api/movies/${movieId}`);
+};
+
 export type Movie = {
   id: number;
   title: string;
@@ -12,4 +16,13 @@ export type Movie = {
   genre: string;
   rating: string;
   posterUrl: string;
+};
+
+type Staff = {
+  name: string;
+  role: string;
+};
+
+export type MovieDetail = Movie & {
+  staff: Staff[];
 };
