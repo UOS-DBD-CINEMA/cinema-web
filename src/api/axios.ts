4efe8ae32f82894daf45c545564c1852af5be1ed
yschoi <yschoi9733@gmail.com>
2025-06-14 03:25:05 +0900

    feat: modify baseURL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,11 +2,9 @@ import axios, { AxiosRequestConfig } from 'axios';
 
 import { getAccessToken } from '@/store/authStore';
 
-const baseURL = import.meta.env.VITE_BASE_URL ?? '';
-
 const createClient = (config?: AxiosRequestConfig) => {
   const axiosInstance = axios.create({
-    baseURL,
+    baseURL: import.meta.env.VITE_BASE_URL,
     timeout: 1000 * 10,
     headers: {
       'Content-Type': 'application/json',
