bf4e037771254d4dd0cf0f4c5f225b4275c4a852
yschoi <yschoi9733@gmail.com>
2025-06-20 14:20:35 +0900

    feat: add movies page

--- src/routes/pages/Movies.tsx
+++ src/routes/pages/Movies.tsx
@@ -14,53 +14,55 @@ export function Movies() {
   return (
     <div className="flex p-4 sm:justify-center sm:py-8">
       <Card className="w-6xl gap-0 p-0">
-        <div className="flex h-full flex-wrap justify-center gap-4 p-2">
-          {movies?.map(movie => (
-            <div key={movie.id} className="flex w-50 flex-col">
-              <div
-                className="relative w-fit"
-                onMouseOver={() => {
-                  setHoveringMovieId(movie.id);
-                }}
-                onMouseOut={() => {
-                  setHoveringMovieId(null);
-                }}
-                onClick={() => {
-                  navigate(`/movies/${movie.id}`);
-                }}
-              >
-                <img
-                  src={movie.posterUrl}
-                  alt={movie.title}
-                  className="aspect-3/4 rounded-2xl"
-                />
-                {hoveringMovieId === movie.id ? (
-                  <div className="absolute top-0 flex h-full w-full flex-col justify-center gap-2 rounded-2xl bg-black/10 p-2">
-                    <Button
-                      className="rounded-xl"
-                      onClick={event => {
-                        event.stopPropagation();
-
-                        navigate(`/ticketing`, {
-                          state: { movieId: movie.id },
-                        });
-                      }}
-                    >
-                      예매하기
-                    </Button>
-                    <Button variant="secondary" className="rounded-xl">
-                      상세보기
-                    </Button>
-                  </div>
-                ) : (
-                  ''
-                )}
+        <div className="flex h-full flex-col items-center gap-4 p-2">
+          <h1 className="text-xl font-semibold">영화 목록</h1>
+          <div className="flex flex-wrap justify-center gap-4">
+            {movies?.map(movie => (
+              <div key={movie.id} className="flex w-50 flex-col">
+                <div
+                  className="relative w-fit"
+                  onMouseOver={() => {
+                    setHoveringMovieId(movie.id);
+                  }}
+                  onMouseOut={() => {
+                    setHoveringMovieId(null);
+                  }}
+                  onClick={() => {
+                    navigate(`/movies/${movie.id}`);
+                  }}
+                >
+                  <img
+                    src={movie.posterUrl}
+                    alt={movie.title}
+                    className="aspect-3/4 rounded-2xl"
+                  />
+                  {hoveringMovieId === movie.id ? (
+                    <div className="absolute top-0 flex h-full w-full flex-col justify-center gap-2 rounded-2xl bg-black/10 p-2">
+                      <Button
+                        className="rounded-xl"
+                        onClick={event => {
+                          event.stopPropagation();
+                          navigate(`/ticketing`, {
+                            state: { movieId: movie.id },
+                          });
+                        }}
+                      >
+                        예매하기
+                      </Button>
+                      <Button variant="secondary" className="rounded-xl">
+                        상세보기
+                      </Button>
+                    </div>
+                  ) : (
+                    ''
+                  )}
+                </div>
+                <div className="flex flex-col overflow-hidden">
+                  <span className="text-center text-xl">{movie.title}</span>
+                </div>
               </div>
-              <div className="flex flex-col overflow-hidden">
-                <span className="text-center text-xl">{movie.title}</span>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </Card>
     </div>
