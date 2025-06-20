97ce0cbfe4b6099e8bd296b2ff764ab7ee74e2fa
yschoi <yschoi9733@gmail.com>
2025-06-21 03:53:12 +0900

    feat: add MovieDetail page

--- src/routes/pages/MovieDetail.tsx
+++ src/routes/pages/MovieDetail.tsx
@@ -1,22 +1,64 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
+
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
+import { useMovie } from '@/hooks/useMovie';
 
 export function MovieDetail() {
   const [movieId, setMovieId] = useState<number>(0);
-  console.log(movieId);
-  //const { data } = useMovie(movieId);
+  const { data: movie } = useMovie(movieId);
+  console.log(movie);
 
   const params = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (params.movieId) {
-      const movieId = parseInt(params.movieId);
-      setMovieId(movieId);
+      setMovieId(parseInt(params.movieId));
     }
   }, [params]);
   return (
-    <>
-      <h1>MovieDetail</h1>
-    </>
+    <div className="flex overflow-auto p-4 sm:justify-center sm:py-8">
+      <Card className="w-6xl gap-0 p-0">
+        <div className="flex h-full flex-col items-center gap-6 p-2">
+          {movie && (
+            <div className="flex flex-col gap-8 lg:w-3xl">
+              <div className="flex flex-wrap items-center gap-2 sm:gap-12">
+                <img
+                  src={movie.posterUrl}
+                  alt={movie.title}
+                  className="aspect-3/4 w-50 rounded-2xl sm:w-80"
+                />
+                <div className="flex flex-col items-start gap-2 sm:scale-120">
+                  <p className="text-2xl font-semibold">{movie.title}</p>
+                  <p>상영 시간: {movie.runtime}분</p>
+                  <p>장르: {movie.genre}</p>
+                  <p>
+                    {movie.staff.map(({ name, role }, i) => (
+                      <span key={i}>
+                        {name}-{role}
+                        &ensp;
+                      </span>
+                    ))}
+                  </p>
+                  <Button
+                    size="sm"
+                    onClick={() => {
+                      navigate(`/ticketing`, {
+                        state: { movieId: movie.id },
+                      });
+                    }}
+                  >
+                    예매하기
+                  </Button>
+                </div>
+              </div>
+              <p>{movie.description}</p>
+            </div>
+          )}
+        </div>
+      </Card>
+    </div>
   );
 }
