f3a98117c6c97fc74e97912de24e660e29918315
yschoi <yschoi9733@gmail.com>
2025-06-21 00:38:18 +0900

    feat :add MovieDetail page

--- src/routes/pages/MovieDetail.tsx
+++ src/routes/pages/MovieDetail.tsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
+import { useMovie } from '@/hooks/useMovie';
+
 export function MovieDetail() {
+  const [movieId, setMovieId] = useState<number>(0);
+  const { data } = useMovie(movieId);
+
+  const params = useParams();
+
+  useEffect(() => {
+    if (params.movieId) {
+      const movieId = parseInt(params.movieId);
+      setMovieId(movieId);
+    }
+  }, [params]);
   return (
     <>
       <h1>MovieDetail</h1>
